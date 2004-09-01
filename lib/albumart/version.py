d7bdda8f18e16b10e00b65cc9d2c6efe734988a6
skyostil <skyostil>
2004-09-01 23:17:35 +0000

    Refactoring.

--- lib/albumart/version.py
+++ lib/albumart/version.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 __program__ = "Album Cover Art Downloader"
 __author__ = "Sami Kyöstilä <skyostil@kempele.fi>"
-__version__ = "1.3.2"
+__version__ = "1.4.0"
 __copyright__ = "Copyright (c) 2003, 2004 by Sami Kyöstilä"
 __license__ = "GPL"
