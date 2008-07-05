aa00f8faa97f46854f4cba1696c829a7c0de8929
U-XP\skyostil <skyostil@xp.(none)>
2008-07-06 01:58:04 +0300

    Win32 binary building fixes.

---
+++ build-win32.sh
@@ -0,0 +1,22 @@
+CX_FREEZE=c:/proj/cx_Freeze-3.0.3/FreezePython.exe
+MAKENSIS=c:/Program\ Files/NSIS/makensis
+
+# Gather the dynamically loaded modules
+MODULES=albumart_dialog,sip,encodings.iso8859_1,encodings.utf_8
+for FN in lib/albumart/albumart_{source,target,recognizer}*.py; do
+  FN=$(basename $FN)
+  FN=${FN/.py/}
+  MODULES=$MODULES,$FN
+done
+
+# Build the frozen binary
+"$CX_FREEZE" --base-name Win32Gui --install-dir dist/win32 --include-path lib/albumart --include-modules $MODULES bin/albumart-qt
+
+# Copy misc files
+cp dist/win32-libs/*.dll dist/win32
+cp copying dist/win32/copying.txt
+cp readme dist/win32/readme.txt
+unix2dos dist/win32/*.txt
+
+# Build installer
+"$MAKENSIS" dist/installer/albumart.nsi
